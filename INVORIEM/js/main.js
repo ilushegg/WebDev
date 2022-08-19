1734f47261485cb3245b4ceeff47aee0a60e485d
ilushegg <marnopolskiyi@mail.ru>
2022-08-19 13:08:06 +0300

    Added video section

--- INVORIEM/js/main.js
+++ INVORIEM/js/main.js
@@ -4,3 +4,21 @@ $(document).ready(function(){
         margin: 48
     });
   });
+
+  const videoBtn = document.querySelector("#video-btn");
+  const videoPicture = document.querySelector(".video__picture");
+  const videoWrapper = document.querySelector(".video__content-inner");
+  const videoFile = document.querySelector("#video-file");
+
+  videoWrapper.addEventListener("click", function () {
+    if (videoFile.paused) {
+        videoPicture.classList.add("none");
+        videoWrapper.classList.remove("video-overlay");
+        videoBtn.classList.add("none")
+        videoFile.play();
+    } else {
+        videoWrapper.classList.add("video-overlay");
+        videoBtn.classList.remove("none");
+        videoFile.pause();
+    }
+  })
