f3969a7dafe9e790e66a6476dda66a6d816dfd9b
ilushegg <marnopolskiyi@mail.ru>
2022-08-25 11:33:58 +0300

    Adaptived the header

--- MAKET/js/main.js
+++ MAKET/js/main.js
@@ -2,6 +2,8 @@ const video = document.querySelector(".video-info__wrapper");
 const videoFile = document.querySelector("#video-file");
 const videoPreview = document.querySelector(".video-preview");
 const videoBtn = document.querySelector(".video-btn");
+const navBtn = document.querySelector(".nav-list__btn");
+let navMenu = document.querySelector(".nav-list__mobile");
 
 video.addEventListener("click", function () {
     if(videoFile.paused){
@@ -14,5 +16,9 @@ video.addEventListener("click", function () {
         videoBtn.classList.remove("none");
         videoFile.pause();
     }
+});
+
+navBtn.addEventListener("click", function () {
+    navMenu.classList.toggle("none");
 });
 
